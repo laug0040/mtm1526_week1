3732bc8f0c68d4a15dc5c808947560cc254626cc
Korey Laughren Bellefeuille <laug0040@algonquinlive.com>
2026-01-16 09:31:22 -0500

    JS Experimentation + HTML & CSS Updates

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
-console.log("Javascript is wild!");
-alert("Welcome to my Week 1 Client-Side Development Assignment!");
+// console.log("Javascript is wild!");
+// alert("Welcome to my Week 1 Client-Side Development Assignment!");
+
+let a = 5
+let b = 10
+
+// console.log(a) // 5
+// console.log(b) // 10
+console.log(a + b) // 15
+
+console.log(a > b) // false
+console.log(a < b) // true
+
+console.log('let a = ' + a)
