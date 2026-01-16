53f35157fec80cc83c63ae77c0c96afae502bff7
Korey Laughren Bellefeuille <laug0040@algonquinlive.com>
2026-01-16 09:39:54 -0500

    JavaScript Concatenation, Interpolation and Operators

--- js/index.js
+++ js/index.js
@@ -7,8 +7,21 @@ let b = 10
 // console.log(a) // 5
 // console.log(b) // 10
 console.log(a + b) // 15
-
 console.log(a > b) // false
 console.log(a < b) // true
 
+// CONCATENATION
 console.log('let a = ' + a)
+console.log('let b = ' + b)
+console.log('a + b = ' + (a + b))
+console.log('a > b = ' + (a > b))
+
+// INTERPOLATION
+console.log(`let a = ${a}`)
+console.log(`a + b = ${a + b}`)
+console.log(`a > b = ${a > b}`)
+
+// OPERATORS
+let n = 10
+n += 1 // n is now 11
+console.log(n)
